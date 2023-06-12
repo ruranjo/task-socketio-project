be3d72b4f572f8525666039cd62d2f6e35b08465
ruranjo <rubens.rangel.job@gmail.com>
2023-06-12 09:50:12 -0400

    public directory was created

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import express from 'express'
 
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express()
+
+app.use(express.static( __dirname + '/public'))
 
 app.listen("3000")
 
+console.log(__dirname)
 console.log("listening port 3000")
