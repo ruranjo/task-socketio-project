5a4c463862340b51403d016b7a107aaed59597c2
ruranjo <rubens.rangel.job@gmail.com>
2023-06-12 11:13:00 -0400

    configured the socket done

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import express from 'express'
+import {Server as WebSocketServer } from 'socket.io'
+import http from 'http'
+
 
 import path from 'path';
 import { fileURLToPath } from 'url';
@@ -6,10 +9,19 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express()
+const server = http.createServer(app)
+const io = new WebSocketServer(server)
 
 app.use(express.static( __dirname + '/public'))
 
-app.listen("3000")
+
+io.on('connection',(socket) => {
+    console.log("conecion establecidad:", socket.id)
+
+    socket.emit("ping")
+})
+
+server.listen("3000")
 
 console.log(__dirname)
 console.log("listening port 3000")
